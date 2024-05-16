682b0ec0dff524d0a7a4aa0b5d40ac42fc1ee7b0
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-05-16 20:59:46 +0700

    Add files via upload

---
+++ BookAmenity.sql
@@ -0,0 +1,17 @@
+﻿USE QLCC 
+
+DELIMITER //
+
+CREATE PROCEDURE BookAmenity (
+  IN tenant_id INT,
+  IN amenity_id INT,
+  IN start_time DATETIME,
+  IN end_time DATETIME
+)
+BEGIN
+      -- Chèn dữ liệu đặt chỗ
+       INSERT INTO Booking (Tenant_ID, Amenity_ID,        Start_Time, End_Time)
+       VALUES (tenant_id, amenity_id, start_time, end_time);
+END//
+DELIMITER;
+
---
+++ Tenant_GetList.sql
@@ -0,0 +1,14 @@
+﻿USE QLCC
+
+DELIMITER //
+
+CREATE PROCEDURE Tenant_GetList()
+BEGIN
+    -- Lấy danh sách cư dân từ các hợp đồng thuê đang hoạt động
+    SELECT Tenant.Tenant_ID, Tenant.Name, Tenant.Phone_Number, Tenant.Email
+    FROM Tenant
+    JOIN Lease ON Tenant.Tenant_ID = Lease.Tenant_ID
+    WHERE Lease.Start_Date <= CURDATE() AND Lease.End_Date >= CURDATE();
+END//
+
+DELIMITER ;
