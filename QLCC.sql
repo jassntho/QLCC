40c02de3a5feb0b7606e73c9c66ad20686beaa26
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-05-16 19:09:40 +0700

    Add files via upload

--- QLCC.sql
+++ QLCC.sql
@@ -3,25 +3,25 @@ USE QLCC
 
 CREATE TABLE Tenant (
 	Tenant_ID INT PRIMARY KEY,
-	Name VARCHAR(50) NOT NULL,
+	Tenant_Name VARCHAR(50) NOT NULL,
 	Phone_Number VARCHAR(10),
 	Email VARCHAR(50),
 	Date_of_Birth DATE,
-	Host INT
+	Unit_Owner INT
 );
 
 CREATE TABLE Amenity (
 	Amenity_ID INT PRIMARY KEY,
-Property_ID INT FOREIGN KEY REFERENCES    Property(Property_ID),
-	Name VARCHAR(50),
-	Description TEXT
+	Property_ID INT FOREIGN KEY REFERENCES Property(Property_ID),
+	Amenity_Name VARCHAR(50),
+	A_Description TEXT
 );
+
 
 CREATE TABLE Booking (
 	Booking_ID INT PRIMARY KEY,
-Tenant_ID INT FOREIGN KEY REFERENCES Tenant(Tenant_ID),
-Amenity_ID INT FOREIGN KEY REFERENCES Amenity(Amenity_ID),
+	Tenant_ID INT FOREIGN KEY REFERENCES Tenant(Tenant_ID),
+	Amenity_ID INT FOREIGN KEY REFERENCES Amenity(Amenity_ID),
 	Start_Time DATETIME,
-	End_Time DATETIME,
-          Cost DECIMAL(10,2)
+	End_Time DATETIME
 );
