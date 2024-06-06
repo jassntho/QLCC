c391a11d37883f1f97b4fb62f14604ef5b05072b
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-06-06 08:10:57 +0700

    view to hide tenant's password

---
+++ View_Password.sql
@@ -0,0 +1,12 @@
+﻿ALTER TABLE Tenant ADD COLUMN Password VARCHAR(255);
+
+CREATE VIEW Tenant_View AS
+SELECT Tenant_ID, Tenant_Name, Phone_Number, Email, Date_of_Birth, Unit_Owner
+FROM Tenant;
+
+-- Thu hồi quyền SELECT trên bảng Tenant từ tenant_role
+REVOKE SELECT ON qlcc.Tenant FROM tenant_role;
+
+-- Cấp quyền SELECT trên view Tenant_View cho tenant_role
+GRANT SELECT ON qlcc.Tenant_View TO tenant_role;
+
