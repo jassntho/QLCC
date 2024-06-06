82a82a2d911c6963e3c292ad385ba5f3aa80451c
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-06-06 08:12:02 +0700

    view to hide the payment cost

---
+++ View_Payment.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW Payment_View AS
+SELECT Payment_ID, Lease_ID, Date, Payment_Type
+FROM Payment;
+
+-- Thu hồi quyền SELECT trên bảng Payment từ tenant_role
+REVOKE SELECT ON qlcc.Payment FROM tenant_role;
+
+-- Cấp quyền SELECT trên view Payment_View cho tenant_role
+GRANT SELECT ON qlcc.Payment_View TO tenant_role;
+
