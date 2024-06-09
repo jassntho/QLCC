4212f3e890da404834818ff70ddf7bb6d48f8d8e
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-06-09 11:23:51 +0700

    Update stored procedure

--- Amenity_Add.sql
+++ Amenity_Add.sql
@@ -1,13 +1,13 @@
-USE QLCC
+USE QLCC;
 
 DELIMITER //
 CREATE PROCEDURE AddAmenity (
-  IN prop_id INT,
+  IN prop_id CHAR(4),
   IN amenity_name VARCHAR(50),
   IN description TEXT
 )
 BEGIN
-  INSERT INTO Amenity (Property_ID, Name, Description)
-  VALUES (property_id, amenity_name, description);
+  INSERT INTO Amenity (Property_ID, Amenity_Name, Description)
+  VALUES (prop_id, amenity_name, description);
 END//
 DELIMITER ;
