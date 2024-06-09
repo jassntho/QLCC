37f8acafe9af4ca305a759c6df8378065bd3f10b
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-06-09 15:44:40 +0700

    Update Amenity_Add them 1 tien ich

--- Amenity_Add.sql
+++ Amenity_Add.sql
@@ -2,7 +2,7 @@ USE QLCC;
 
 DELIMITER //
 
-CREATE PROCEDURE AddAmenityAndUpdateCost(
+CREATE PROCEDURE AddAmenity (
     IN p_Amenity_ID CHAR(4),
     IN p_Property_ID CHAR(4),
     IN p_Amenity_Name VARCHAR(50),
