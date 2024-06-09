2b6614b82a603fc5f4da886727edccb7aaa1ffed
hongkhanh1310 <163385893+hongkhanh1310@users.noreply.github.com>
2024-06-09 15:34:16 +0700

    Update Amenity_Add them 1 tien ich

--- Amenity_Add.sql
+++ Amenity_Add.sql
@@ -1,13 +1,16 @@
 USE QLCC;
 
 DELIMITER //
-CREATE PROCEDURE AddAmenity (
-  IN prop_id CHAR(4),
-  IN amenity_name VARCHAR(50),
-  IN description TEXT
+
+CREATE PROCEDURE AddAmenity(
+    IN p_Amenity_ID CHAR(4),
+    IN p_Property_ID CHAR(4),
+    IN p_Amenity_Name VARCHAR(50),
+    IN p_Description TEXT
 )
 BEGIN
-  INSERT INTO Amenity (Property_ID, Amenity_Name, Description)
-  VALUES (prop_id, amenity_name, description);
-END//
-DELIMITER;
+    INSERT INTO Amenity (Amenity_ID, Property_ID, Amenity_Name, Description)
+    VALUES (p_Amenity_ID, p_Property_ID, p_Amenity_Name, p_Description);
+END //
+
+DELIMITER ;
